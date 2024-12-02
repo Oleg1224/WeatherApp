35a072336f613afd3191477743623a809122d1de
Oleg1224 <olegik.kr@gmail.com>
2024-12-02 15:45:00 +0200

    1

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ function allInfoWeather() {
         else {
             div.classList.remove('new')
         }
-        this.blur()
+        input.blur()
         /* if(div.style.transform.scale=1) {
             div.classList.add('new')
         } */
